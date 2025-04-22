a24cd3488c31320351a6ea1a936c90f5924eee74
TranHPhuoc <tranhuuphuoc1302kt@gmail.com>
2025-04-22 11:21:05 +0700

    build1

--- src/types/movie.ts
+++ src/types/movie.ts
@@ -58,7 +58,7 @@ export interface UpcomingMovies {
 }
 
 // API Endpoints
-const API_URL = 'http://localhost:8080/api/v1';
+const API_URL = 'http://movieweb-production.up.railway.app/api/v1';
 
 export const movieApi = {
     getPopularMovies: async (page: number = 1): Promise<PopularMoviesResponse> => {
